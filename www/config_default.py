73fc62bb3af6fcedcb7615eb2f52dd71f0ef78c1
Minty <mintyck@gmail.com>
2018-03-13 23:13:40 +1100

    Added 3 config files

---
+++ www/config_default.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+'''
+Default configurations.
+'''
+
+__author__ = 'Minty'
+
+configs = {
+    'debug': True,
+    'db': {
+        'host': '127.0.0.1',
+        'port': 3306,
+        'user': 'root',
+        'password': 'password',
+        'db': 'awesome'
+    },
+    'session': {
+        'secret': 'Awesome'
+    }
+}
---
+++ www/config_override.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+'''
+Override configurations.
+'''
+
+__author__ = 'Minty'
+
+configs = {
+    'db': {
+        'host': '127.0.0.1'
+    }
+}
