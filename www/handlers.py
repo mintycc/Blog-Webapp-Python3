e65687b5a7dc4d8f6f27db5c8f89db9d537b1f6b
Minty <mintyck@gmail.com>
2018-03-14 12:00:34 +1100

    Blog handler added in handlers.py

--- www/handlers.py
+++ www/handlers.py
@@ -3,13 +3,33 @@
 __author__ = 'Minty'
 
 from coroweb import get, post
-from model import User
-import asyncio
+from model import User, Blog, Comment
+import asyncio, time
 
 @get('/')
 async def index(request):
-    users = await User.findAll()
+    summary = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+    blogs = [
+        Blog(
+            id = '1', 
+            name = 'Test Blog', 
+            summary = summary, 
+            created_at = time.time() - 120
+        ),
+        Blog(
+            id = '2', 
+            name = 'Something New', 
+            summary = summary, 
+            created_at = time.time() - 3600
+        ),
+        Blog(
+            id = '3', 
+            name = 'Learn Swift', 
+            summary = summary, 
+            created_at = time.time() - 7200
+        )
+    ]
     return {
-        '__template__': 'test.html',
-        'users': users
+        '__template__': 'blogs.html',
+        'blogs': blogs
     }
